bf4bfd6db325851683763930725aff1f0bccc47c
Siwon Kim <siwonkim@zuminternet.com>
2022-12-08 11:08:21 +0900

    Initial Commit

---
+++ Jane1208/Jane1208App.swift
@@ -0,0 +1,17 @@
+//
+//  Jane1208App.swift
+//  Jane1208
+//
+//  Created by Siwon Kim on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct Jane1208App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
